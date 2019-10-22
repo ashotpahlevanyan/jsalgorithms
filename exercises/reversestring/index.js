9055adb34e9d481ac63a0c84341296fe6cc99d4e
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-10-22 16:05:29 +0400

    reverse string st 3 sol

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,42 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reverse().join('');
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed;
+    }, '');
 }
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     let reversed = '';
+//
+//     for(let character of str) {
+//         reversed = character + reversed;
+//     }
+//
+//     return reversed;
+// }
+
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//
+//     return arr.join('');
+//
+// }
+
+// function reverse(str) {
+//     let res = '';
+//     for(let i = str.length - 1; i > -1; i--) {
+//         res = res + str[i];
+//     }
+//
+//     return res;
+//
+// }
