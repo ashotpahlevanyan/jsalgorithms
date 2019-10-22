2dbd9627f4d77a95fd95da0b551e13a82d55ad39
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-10-22 17:36:44 +0400

    palindrome non effective

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -10,7 +10,7 @@
 function palindrome(str) {
     return str.split('').every((char, index) => {
         return char === str[str.length - index - 1];
-    })
+    }); // has a problem of doing last half comparision twice
 }
 
 module.exports = palindrome;
