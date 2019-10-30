825570f35df2f6f97d732866257a54e0dd590d03
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-10-30 19:14:11 +0400

    anagrams sg first solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+const word = "HI THERE!!!!!";
+word.replace(/[^\w]/g, "").toLowerCase();
+
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+
+    for(let char in aCharMap) {
+        if(aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function buildCharMap(string) {
+    const charMap = {};
+    for(let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
 
 module.exports = anagrams;
