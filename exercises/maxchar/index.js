40849b01edd927d1342e42cbf2c18f18427622ed
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-10-30 15:52:12 +0400

    maxchar solved

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -13,7 +13,26 @@ for(let char of string) {
 }
 
 function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
 
+    for(let char of str) {
+        if(charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    for(let char in charMap) {
+        if(charMap.hasOwnProperty(char) && charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
 }
 
 module.exports = maxChar;
