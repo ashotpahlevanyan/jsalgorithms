a08927536854ebe76739f87351e740667e9f9014
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-10-31 14:39:14 +0400

    steps my solution and sg first solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,18 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for(let i = 0; i < n; i++ ) {
+        let step = '';
+        for(let j = 0; j < n; j++) {
+            if(j <= i) {
+                step += '#';
+            } else {
+                step += ' ';
+            }
+        }
+        console.log(step);
+    }
+}
 
 module.exports = steps;
