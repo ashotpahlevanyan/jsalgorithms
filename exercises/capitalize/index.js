024ea80eab45bd25f35289514e734dd43471fa25
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-10-31 13:17:50 +0400

    capitalize sg second solution, which is simple but not the best looking, though passes 4th test

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,6 +8,25 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for(let i = 1; i < str.length; i++) {
+        if(str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+
+module.exports = capitalize;
+
+
+
+/*
+function capitalize(str) {
     const words = [];
 
     for(let word of str.split(' ')) {
@@ -20,8 +39,6 @@ function capitalize(str) {
 module.exports = capitalize;
 
 
-
-/*
 * function capitalizeWord(word) {
     return word[0].toUpperCase() + word.slice(1);
 }
