4def6848987c5213925b653fa2e4adca8d6c84f9
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-11-01 15:06:17 +0400

    vowels sg solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let count = 0;
+    const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    for(let char of str.toLowerCase().split('')) {
+        if(checker.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
 
 module.exports = vowels;
